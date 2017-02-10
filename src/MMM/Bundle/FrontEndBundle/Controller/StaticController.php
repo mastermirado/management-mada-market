0a52d3626880f9a7023e857bbb0282803f82d90f
Mirado <mirado.rakotoarison@netapsys.fr>
2017-02-10 10:41:51 +0300

    fractionnement contenu static

---
+++ src/MMM/Bundle/FrontEndBundle/Controller/StaticController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mirado Rakotoarison
+ * Date: 10/02/2017
+ * Time: 10:33
+ */
+
+namespace MMM\Bundle\FrontEndBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class StaticController extends Controller
+{
+    /**
+     * @Template()
+     */
+    public function menuAction(){
+    }
+}
